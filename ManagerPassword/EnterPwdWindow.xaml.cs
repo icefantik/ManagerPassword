323d407a395aad9d0baf8005f65d4e76e3a69e21
petya <mityushin_petya@mail.ru>
2023-01-03 15:52:29 +0300

    minor revisions

--- ManagerPassword/EnterPwdWindow.xaml.cs
+++ ManagerPassword/EnterPwdWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace ManagerPassword
         {
             InitializeComponent();
         }
+
+        private void EnterPwdBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
