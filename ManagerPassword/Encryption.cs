bee553e349cf1be8157d75599aa5cab3f7ba8b13
petya <mityushin_petya@mail.ru>
2023-01-05 20:11:46 +0300

    start fix bug

--- ManagerPassword/Encryption.cs
+++ ManagerPassword/Encryption.cs
@@ -11,18 +11,23 @@ namespace ManagerPassword
 {
     internal class Encryption
     {
-        private static long p = 5, q = 9, n, m, d, e;
+        private static long p = 5, q = 7, n, m, d, e;
         private static char[] characters = new char[] { '#', 
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
         public static string EncryptionPwd(string pwd)
         {
-            n = p * q;
-            m = (p - 1) * (q - 1);
-            d = CalculateD(m);
-            e = CalculateE(d, m);
+            if (IsTheNumberSimple(p) && IsTheNumberSimple(q))
+            {
+                n = p * q;
+                m = (p - 1) * (q - 1);
+                d = CalculateD(m);
+                e = CalculateE(d, m);
 
-            return RSAEncoder(pwd, e, n);
+                return RSAEncoder(pwd, e, n);
+            }
+            else
+                return "";
         }
         public static string DecryptionPwd(string hashPwd)
         {
@@ -76,6 +81,19 @@ namespace ManagerPassword
             }
             return e;
         }
+
+        private static bool IsTheNumberSimple(long n)
+        {
+            if (n < 2)
+                return false;
+            if (n == 2)
+                return true;
+            for (long i = 2; i < n; i++)
+                if (n % i == 0)
+                    return false;
+            return true;
+        }
+
         public static string RSADecryption(string hashPwd, long d, long n)
         {
             string[] hashPwdArray = hashPwd.Split('-');
